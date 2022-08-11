3a3ab53d0afcda828025abb3cf0b1b11132ee099
Avrel Owona <guyowona01@gmail.com>
2022-08-11 12:16:57 +0100

    Contact

--- pages/tlp_tv.js
+++ pages/tlp_tv.js
@@ -4,7 +4,7 @@ const TlpTv = () => {
     return (
         <div className='h-screen w-screen relative'>
             <div className="h-full w-full justify-center items-center absolute top-0 flex flex-col z-40">
-                <h3 className='text-white font-bold text-6xl uppercase text-center'>Notre Télévision, bientôt <br/>disponible</h3>
+                <h3 className='text-white font-bold text-3xl sm:text-6xl uppercase text-center'>Notre Télévision, bientôt <br/>disponible</h3>
                 {/*<p className='pt-10 w-10/12 lg:w-4/12 text-white text-xl font-light text-center lg:text-left'>Tournons La Page est un mouvement citoyen international qui agit pour la promotion de l’alternance démocratique en Afrique subsaharienne.</p>*/}
             </div>
             <div className='h-full w-full bg-black opacity-50 absolute top-0 border-red-500 top-0'></div>
