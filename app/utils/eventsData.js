22f6b0ceb157480581b212f42ab83f53707a0ce1
Avrel Owona <guyowona01@gmail.com>
2022-08-29 11:52:42 +0100

    articles

--- app/utils/eventsData.js
+++ app/utils/eventsData.js
@@ -2,7 +2,7 @@ export const EventsData =  [
     {
         link : '#',
         img : '/1.jpeg',
-        title : 'L’Assemblée Générale de Tournons La Page Cameroun est prévue pour le mois d’Octobre 2022. A travers l’enquête en cours nous aimerions savoir ce que vous pensez de cette coalition depuis sa mise en place et le lancement de ses activités le 15 Septembre 2015 puis tu mets télécharger le formulaire ci-joint',
+        title : 'L’Assemblée Générale de Tournons La Page Cameroun est prévue pour le mois d’Octobre 2022. A travers l’enquête en cours nous aimerions savoir ce que vous pensez de cette coalition depuis sa mise en place et le lancement de ses activités le 15 Septembre 2015',
         textButton : 'Formulaire',
         fileSrc : 'tlp-cmr-draft-enquete.docx',
         id : 1
