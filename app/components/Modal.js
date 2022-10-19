536ace1cbca8a045ec2b7a45e4aa5b0f40e2f7e8
Avrel-Owona <guyowona01@gmail.com>
2022-10-19 15:35:14 +0100

    Modal

--- app/components/Modal.js
+++ app/components/Modal.js
@@ -6,11 +6,11 @@ const Modal = ({modal, setModal, imgSrc}) => {
     return (
         <div className={`${modal ? 'scale-100 opacity-1 visible modal fixed top-0 left-0 w-full h-screen bg-black flex items-center justify-center overflow-hidden' : 'opacity-0 hidden'}`}>
                 <button className='border justify-center items-center flex fixed text-white text-2xl top-10 right-10 cursor-pointer' onClick={()=>setModal(false)}>
-                    <AiOutlineClose/>
+                    <AiOutlineClose/> 
                 </button>
                 <img className='h-auto max-h-full w-auto max-w-full py-10' src={imgSrc} alt=""/>
             </div>
     )
 }
 
-export default Modal;
+export default Modal;  
