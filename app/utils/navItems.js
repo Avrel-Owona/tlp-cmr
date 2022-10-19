684a5d40dd405fe638d56f7d9551bfa6a36760e0
Avrel-Owona <guyowona01@gmail.com>
2022-10-19 15:08:28 +0100

    items pathname

--- app/utils/navItems.js
+++ app/utils/navItems.js
@@ -174,6 +174,7 @@ export const NewsItemsCards = [
         type : "Article",
         title : "Liberez Oumar SYLLA et Ibrahima DIALLO, activistes pro-démocratie guinéens !",
         cover : "/liberez_oumar_etibrahima/PHOTO-2022-09-26-16-30-13.jpg",
+        path : "liberez_oumar_et_ibrahima",
         date : "26 Septembre 2022",
         picturesGalery : true,
         pictures : [
@@ -185,6 +186,7 @@ export const NewsItemsCards = [
         id : 3,
         type : "Article",
         title : "Rencontre avec les OSC à Ngaoundéré",
+        path : "rencontre_avec_osc_ngaoundere",
         cover : "/rencontre_avec_les_osc_ngaoundere/PHOTO-2022-09-22-15-06-00.jpg",
         date : "22 Septembre 2022",
         picturesGalery : true,
@@ -204,6 +206,7 @@ export const NewsItemsCards = [
         type : "Article",
         title : "Rencontre d'échange, de sensibilisation et de mobilisation des OSC et personnalité du Nord | Siège de CADPEN à Garoua",
         cover : "/rencontre_d_echange/PHOTO-2022-09-21-13-40-11 2.jpg",
+        path : "echange_sur_la_sensibilisation_des_osc",
         date : "21 Septembre 2022",
         picturesGalery : true,
         pictures : [
@@ -221,6 +224,7 @@ export const NewsItemsCards = [
         title : "Journée internationale de la démocratie | Live Facebook",
         cover : "/journee_internationale_de_la_democratie/PHOTO-2022-09-15-13-44-44.jpg",
         date : "21 Septembre 2022",
+        path : "journee_international_de_la_democratie",
         picturesGalery : true,
         pictures : [
             {imgSrc : "/journee_internationale_de_la_democratie/PHOTO-2022-09-15-13-44-44.jpg"},
@@ -232,6 +236,7 @@ export const NewsItemsCards = [
         title : "Mission de sensibilisation et de mobilisation | Douala & Limbe",
         cover : "/dla_12_09_22/PHOTO-2022-09-13-18-14-03.jpg",
         date : "13 Septembre 2022",
+        path : "mission_de_sensibilisation",
         picturesGalery : true,
         pictures : [
             {imgSrc : "/dla_12_09_22/PHOTO-2022-09-13-18-14-43.jpg"},
@@ -253,6 +258,7 @@ export const NewsItemsCards = [
     {
         id : 7,
         type : "Arcticle",
+        path : "grand_concert_panafricain",
         title : "GRAND CONCERT PANAFRICAIN pour la Limitation2Mandats",
         description : 'Campagne international pour la limitation des mandats :  Le GRAND CONCERT PANAFRICAIN pour la Limitation2Mandats réunira 11 artistes africains de 7 pays différents, pour célébrer la #démocratie, la bonne #gouvernance et le respect des constitutions africaines. Les artites : Meiway, Didier Awadi, Elom 20ce, Papy Kerro, Nourrath la Debboslam, Nanda, Amen Jah Cissé, Léman, Don Stash, Lyne des Mots et Momo Kankua.',
         cover : "/campagne.jpeg",
@@ -262,6 +268,7 @@ export const NewsItemsCards = [
         id : 8,
         type : "Article",
         title : "Atelier Monastère",
+        path : "atelier_monastere",
         cover : "/atelier_monastère/IMG-20211016-WA0002.jpg",
         date : "20 Octobre 2021",
         picturesGalery : true,
@@ -288,6 +295,7 @@ export const NewsItemsCards = [
         title : "Atelier de femmes Leaders et defenseures des droits humains au Cameroun",
         cover : "/ateliers_des_femmes_leaders/IMG_20211202_113140.jpg",
         date : "02-03 Décembre 2021",
+        path : "atelier_des_femmes_leaders",
         picturesGalery : true,
         pictures : [
             {imgSrc : '/ateliers_des_femmes_leaders/IMG_20211202_102338.jpg'},
@@ -308,6 +316,7 @@ export const NewsItemsCards = [
         id : 10,
         type : "Communique",
         title : "Atelier de formation numérique",
+        path : "atelier_de_formation_numerique",
         cover : '/atelier_formation_numérique/IMG_20210924_104837.jpg',
         date : "02-03 Décembre 2021",
         picturesGalery : true,
@@ -326,6 +335,7 @@ export const NewsItemsCards = [
         title : "Atelier national du pool des avocats pour la protection et la defense des membres de TLP CMR",
         cover : '/atelier_mise_en_place_Pool_des_avocats/IMG_20200814_124555.jpg',
         date : "14 Août 2020",
+        path : "atelier_national_du_pool_des_avocats_tlp_cmr",
         picturesGalery : true,
         pictures : [
             {imgSrc : '/atelier_mise_en_place_Pool_des_avocats/IMG_20200814_124555.jpg'},
@@ -345,6 +355,7 @@ export const NewsItemsCards = [
         type : "Article",
         title : "Jean-Marc BIKOKO | Le combat dans l'âme - CAMEROUN",
         cover : '/combat_dans_l_ame/JMB.png',
+        path : "combat_dans_l_ame_jean_marc_bikoko",
         date : "07 Juillet 2016",
         // description : 'Le 15 septembre 2015, journée internationale de la démocratie, des membres de la coalition Tournons la Page au Cameroun ont été brutalement arrêtés lors d’un atelier de travail. Cette arrestation est intervenue alors que le réseau Dynamique Citoyenne (coordinateur national de Tournons la Page) était réuni au Palais des sports de Yaoundé dans le cadre d’un atelier de réflexion sur la gouvernance électorale et l’alternance démocratique. Sur l’ordre du sous-préfet de Yaoundé 2ème, les membres présents de la campagne Tournons la Page au Cameroun ont été séquestrés puis évacués violemment, par une quarantaine d’éléments des forces de l’ordre. Les T-shirts des participants ont été arrachés, le matériel des journalistes présents à l’événement et les téléphones de certains participants ont été confisqués. Six personnes ont été arrêtées ce 15 septembre 2015, journée internationale de la démocratie. Il s’agit de : M. Jean-Marc BIKOKO (le point focal national de Dynamique Citoyenne, Président de la Centrale Syndicale du Secteur Public),Agnès Adélaïde METOUGOU, BIKOKO Le juste, Jessie BIKOKO, NDJALLA EPANGUE Yves et FOGNO FOTSO François.Ils sont actuellement toujours en détention. De plus, le 16 septembre dans la matinée, la police a fait irruption au siège de Dynamique Citoyenne, arrêtant notamment son coordinateur, Léon Ngaba, ainsi que Jean-Baptiste Sipa, coordinateur national de Article 55 alors que le réseau tenait sa coordination nationale. La coalition internationale Tournons la Page dénonce avec la plus grande véhémence ces atteintes graves et flagrantes à la liberté d’expression et interpelle l’opinion publique internationale sur ces actes de répression et d’intimidation à l’encontre de la société civile camerounaise, en particulier les membres de la campagne Tournons la page, qui exerce sa fonction d’observation, de réflexion et d’analyse citoyennes de la société. L’ensemble de la coalition internationale Tournons la Page, qui réunit une cinquantaine d’organisations en Afrique et en Europe ainsi qu’un bon millier de soutiens, demande la libération immédiate et sans condition de ses membres arbitrairement détenus. La Campagne en appelle au sens de responsabilité des pouvoirs publics et leur devoir de garantir en toutes circonstances l’intégrité physique et psychologique nécessaire à la société civile afin qu’elle mène librement son action citoyenne. Tournons la Page demande aux Nations Unies, à l’Union Européenne, ainsi qu’à l’Union Africaine de tout mettre en oeuvre en vue d’obtenir la libération des personnes détenues.',
         picturesGalery : true,
@@ -367,6 +378,7 @@ export const NewsItemsCards = [
         title : "L’activiste de TLP CMR Jean-Marc BIKOKO | Interview France 24",
         cover : '/interview_fr_24/IMG_20210927_173804.jpg',
         date : "14-15 Septembre 2015",
+        path : "interview_fr_24_jean_marc_bikoko",
         // description : 'Le 15 septembre 2015, journée internationale de la démocratie, des membres de la coalition Tournons la Page au Cameroun ont été brutalement arrêtés lors d’un atelier de travail. Cette arrestation est intervenue alors que le réseau Dynamique Citoyenne (coordinateur national de Tournons la Page) était réuni au Palais des sports de Yaoundé dans le cadre d’un atelier de réflexion sur la gouvernance électorale et l’alternance démocratique. Sur l’ordre du sous-préfet de Yaoundé 2ème, les membres présents de la campagne Tournons la Page au Cameroun ont été séquestrés puis évacués violemment, par une quarantaine d’éléments des forces de l’ordre. Les T-shirts des participants ont été arrachés, le matériel des journalistes présents à l’événement et les téléphones de certains participants ont été confisqués. Six personnes ont été arrêtées ce 15 septembre 2015, journée internationale de la démocratie. Il s’agit de : M. Jean-Marc BIKOKO (le point focal national de Dynamique Citoyenne, Président de la Centrale Syndicale du Secteur Public),Agnès Adélaïde METOUGOU, BIKOKO Le juste, Jessie BIKOKO, NDJALLA EPANGUE Yves et FOGNO FOTSO François.Ils sont actuellement toujours en détention. De plus, le 16 septembre dans la matinée, la police a fait irruption au siège de Dynamique Citoyenne, arrêtant notamment son coordinateur, Léon Ngaba, ainsi que Jean-Baptiste Sipa, coordinateur national de Article 55 alors que le réseau tenait sa coordination nationale. La coalition internationale Tournons la Page dénonce avec la plus grande véhémence ces atteintes graves et flagrantes à la liberté d’expression et interpelle l’opinion publique internationale sur ces actes de répression et d’intimidation à l’encontre de la société civile camerounaise, en particulier les membres de la campagne Tournons la page, qui exerce sa fonction d’observation, de réflexion et d’analyse citoyennes de la société. L’ensemble de la coalition internationale Tournons la Page, qui réunit une cinquantaine d’organisations en Afrique et en Europe ainsi qu’un bon millier de soutiens, demande la libération immédiate et sans condition de ses membres arbitrairement détenus. La Campagne en appelle au sens de responsabilité des pouvoirs publics et leur devoir de garantir en toutes circonstances l’intégrité physique et psychologique nécessaire à la société civile afin qu’elle mène librement son action citoyenne. Tournons la Page demande aux Nations Unies, à l’Union Européenne, ainsi qu’à l’Union Africaine de tout mettre en oeuvre en vue d’obtenir la libération des personnes détenues.',
         picturesGalery : true,
         pictures : [
@@ -388,6 +400,7 @@ export const NewsItemsCards = [
         title : "Arrestation des leaders Camerounais lors du lancement de TLP Cameroun",
         cover : '/arrestation_des_leaders_amerounais/arrestation_bikoko_jean_marc.png',
         date : "14-15 Septembre 2015",
+        path : "arrestation_des_leaders_tlp_cmr",
         description : 'Le 15 septembre 2015, journée internationale de la démocratie, des membres de la coalition Tournons la Page au Cameroun ont été brutalement arrêtés lors d’un atelier de travail. Cette arrestation est intervenue alors que le réseau Dynamique Citoyenne (coordinateur national de Tournons la Page) était réuni au Palais des sports de Yaoundé dans le cadre d’un atelier de réflexion sur la gouvernance électorale et l’alternance démocratique. Sur l’ordre du sous-préfet de Yaoundé 2ème, les membres présents de la campagne Tournons la Page au Cameroun ont été séquestrés puis évacués violemment, par une quarantaine d’éléments des forces de l’ordre. Les T-shirts des participants ont été arrachés, le matériel des journalistes présents à l’événement et les téléphones de certains participants ont été confisqués. Six personnes ont été arrêtées ce 15 septembre 2015, journée internationale de la démocratie. Il s’agit de : M. Jean-Marc BIKOKO (le point focal national de Dynamique Citoyenne, Président de la Centrale Syndicale du Secteur Public),Agnès Adélaïde METOUGOU, BIKOKO Le juste, Jessie BIKOKO, NDJALLA EPANGUE Yves et FOGNO FOTSO François.Ils sont actuellement toujours en détention. De plus, le 16 septembre dans la matinée, la police a fait irruption au siège de Dynamique Citoyenne, arrêtant notamment son coordinateur, Léon Ngaba, ainsi que Jean-Baptiste Sipa, coordinateur national de Article 55 alors que le réseau tenait sa coordination nationale. La coalition internationale Tournons la Page dénonce avec la plus grande véhémence ces atteintes graves et flagrantes à la liberté d’expression et interpelle l’opinion publique internationale sur ces actes de répression et d’intimidation à l’encontre de la société civile camerounaise, en particulier les membres de la campagne Tournons la page, qui exerce sa fonction d’observation, de réflexion et d’analyse citoyennes de la société. L’ensemble de la coalition internationale Tournons la Page, qui réunit une cinquantaine d’organisations en Afrique et en Europe ainsi qu’un bon millier de soutiens, demande la libération immédiate et sans condition de ses membres arbitrairement détenus. La Campagne en appelle au sens de responsabilité des pouvoirs publics et leur devoir de garantir en toutes circonstances l’intégrité physique et psychologique nécessaire à la société civile afin qu’elle mène librement son action citoyenne. Tournons la Page demande aux Nations Unies, à l’Union Européenne, ainsi qu’à l’Union Africaine de tout mettre en oeuvre en vue d’obtenir la libération des personnes détenues.',
         picturesGalery : true,
         pictures : [
@@ -408,6 +421,7 @@ export const NewsItemsCards = [
         title : "L’activiste de TLP CMR Jean-Marc BIKOKO arrêté et toujours détenu à Yaoundé",
         cover : "/arrestation_jean_marc_bikoko/jean_marc_bikoko.jpeg",
         date : "15 Septembre 2015",
+        path : "jean_marc_bikoko_arrete",
         author : 'Mathieu Olivier',
         description : "Arrêté mardi 15 septembre au Palais des Sports de Yaoundé en compagnie de cinq autres personnes, dont le journaliste François Fogno Fotso, Jean-Marc Bikoko avait été emmené au commissariat central de Yaoundé, en fin de matinée mercredi 16 septembre, afin d’y être interrogé sur le colloque auquel il participait. Selon Emmanuel Edou, avocat de Jean-Marc Bikoko, contacté par Jeune Afrique, les policiers lui ont ensuite signifié en fin d’après-midi qu’il faisait l’objet d’une garde à vue de quinze jours, renouvelable une fois sur décret du gouverneur du Centre. Si son avocat espère encore obtenir une libération, l’activiste devrait donc rester au minimum deux semaines de plus en détention. Ce cas de figure est relativement rare dans la législation camerounaise puisque, selon son avocat, une telle durée de garde à vue n’existe que dans le cadre de la nouvelle loi anti-terroriste et dans celui de la loi sur le maintien de l’ordre de 1990. Défaut d’autorisation ? Point focal national du Réseau dynamique citoyenne, coordinateur de la Plateforme d’information et d’action sur la dette du Cameroun, président confédéral de la Centrale syndicale du secteur public (CSP), Jean-Marc Bikoko prenait part, au moment de son arrestation, à un « atelier de lancement de la campagne ‘Tournons La Page’ au Cameroun », ayant pour thème « gouvernance électorale et alternance démocratique » et organisé à l’occasion de la célébration de la Journée internationale de la démocratie. « Le sous-préfet est venu au Palais des Sports pour leur interdire le colloque et les policiers ont aussi investi les lieux », explique Emmanuel Edou. Interpellant six personnes, ils les auraient notamment dépouillés de leur t-shirt, sur lesquels était inscrit un slogan pour l’alternance démocratique au Cameroun. Les forces de l’ordre estiment que Jean-Marc Bikoko et son organisation n’avait pas obtenu l’autorisation de tenir cet événements. L’intéressé a quant à lui répondu aux enquêteurs que, ne s’agissant pas d’une manifestation mais d’un colloque, il avait estimé ne pas avoir besoin d’un tel sésame des autorités, d’autant plus que le Palais des Sports avait été loué. La campagne pour l’alternance organisée par le Réseau dynamique citoyenne est censé se poursuivre jusqu’en 2018, année au cours de laquelle le pays organisera les élections municipales, législatives, sénatoriales, régionales et présidentielle.",
     },
@@ -417,6 +431,7 @@ export const NewsItemsCards = [
         title : "Tournons La Page Cameroun - Pas de démocratie sans alternance",
         cover : '/tlp _as_de_democratie_sans_alternance/tlp_cmr.png',
         date : "05 Avril 2015",
+        path : "pas_de_democratie_sans_alternance",
         description : "Tournons la page a lancé son plaidoyer à Paris et à Bruxelles début février 2015. Mgr Fridolin Ambongo et l’Abbé Santedi de la commission Justice et Paix de la Conférence Episcopale Nationale du Congo (CENCO), de la République Démocratique du Congo, ainsi que Brice Mackosso, le secrétaire général de la commission Justice et Paix de Pointe-Noire a pour le Congo Brazzaville, ont été invités à Paris et ont pu rencontrer des représentants du gouvernement et de la présidence française. L’église catholique congolaise a alors réclamé un soutien européen à la démocratie en Afrique.",
         picturesGalery : true,
         pictures : [
@@ -435,6 +450,7 @@ export const NewsItemsCards = [
         type : "Arcticle",
         title : "L’activiste de TLP CMR Jean-Marc BIKOKO | Interview France 24",
         author : 'Mathieu Olivier',
+        path : "convocation_2e_assemblee_generale",
         description : "Arrêté mardi 15 septembre au Palais des Sports de Yaoundé en compagnie de cinq autres personnes, dont le journaliste François Fogno Fotso, Jean-Marc Bikoko avait été emmené au commissariat central de Yaoundé, en fin de matinée mercredi 16 septembre, afin d’y être interrogé sur le colloque auquel il participait. Selon Emmanuel Edou, avocat de Jean-Marc Bikoko, contacté par Jeune Afrique, les policiers lui ont ensuite signifié en fin d’après-midi qu’il faisait l’objet d’une garde à vue de quinze jours, renouvelable une fois sur décret du gouverneur du Centre. Si son avocat espère encore obtenir une libération, l’activiste devrait donc rester au minimum deux semaines de plus en détention. Ce cas de figure est relativement rare dans la législation camerounaise puisque, selon son avocat, une telle durée de garde à vue n’existe que dans le cadre de la nouvelle loi anti-terroriste et dans celui de la loi sur le maintien de l’ordre de 1990.",
         cover : "/0a3fac1b-6bc3-4670-affe-9fbe24eedfad.JPG",
         date : "04 Juillet 2016",
