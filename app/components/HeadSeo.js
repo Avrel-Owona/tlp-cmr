e0b76ee7ba669ff0e443279244be51da8f6b8775
Avrel-Owona <guyowona01@gmail.com>
2022-10-20 20:30:50 +0100

    update

--- app/components/HeadSeo.js
+++ app/components/HeadSeo.js
@@ -1,30 +1,37 @@
+import React from "react"
 import Head from "next/head"
 
 
 const HeadSeo = ({pageTitle, description, currentURL, previewImage}) =>{
+    console.log('infos', {
+        pageTitle : pageTitle,
+        description : description,
+        currentURL : currentURL,
+        previewImage : previewImage
+    })
 
     return (
         <Head>
-            <title>Tournons La Page Cameroun | {pageTitle}</title>
-            <meta name="description" content={description} />
-            <meta property="og:title" content={pageTitle} key="ogtitle" />
-            <meta property="og:description" content={description} key="ogdesc" />
+                <title>Tournons La Page Cameroun | {pageTitle}</title>
+                <meta name="description" content={description} />
+                <meta property="og:title" content={pageTitle} key="ogtitle" />
+                <meta property="og:description" content={description} key="ogdesc" />
 
-            {/* Twitter */}
-            <meta name="twitter:site" content='tournonslapagecameroun.org' />
-            <meta name="twitter:title" content={pageTitle} />
-            <meta name="twitter:image:src" content={previewImage} />
-            <meta name="twitter:card" content="summary_large_image" key="twcard" />
-            <meta name="twitter:creator" key="twhandle" />
-            <meta name="twitter:description" content={description} />
+                {/* Twitter */}
+                <meta name="twitter:site" content='tournonslapagecameroun.org' />
+                <meta name="twitter:title" content={pageTitle} />
+                <meta name="twitter:image:src" content={previewImage} />
+                <meta name="twitter:card" content="summary_large_image" key="twcard" />
+                <meta name="twitter:creator" key="twhandle" />
+                <meta name="twitter:description" content={description} />
 
-            {/* Open Graph */}
-            <meta property="og:type" content='openGraphType' key="ogurl" />
-            <meta property="og:url" content={`tournonslapagecameroun.org${currentURL}`} key="ogurl" />
-            <meta property="og:image" content={previewImage} key="ogimage" />
-            <meta property="og:site_name" content='tournonslapagecameroun.org' key="ogsitename" />
-            <meta property="og:title" content={pageTitle} key="ogtitle" />
-            <meta property="og:description" content={description} key="ogdesc" />
+                {/* Open Graph */}
+                <meta property="og:type" content='openGraphType' key="ogurl" />
+                <meta property="og:url" content={`tournonslapagecameroun.org${currentURL}`} key="ogurl" />
+                <meta property="og:image" content={previewImage} key="ogimage" />
+                <meta property="og:site_name" content='tournonslapagecameroun.org' key="ogsitename" />
+                <meta property="og:title" content={pageTitle} key="ogtitle" />
+                <meta property="og:description" content={description} key="ogdesc" />
         </Head>
     )
 }
