328c3824e9b726bdb09124e4426a7b66d5cd7a84
Avrel-Owona <guyowona01@gmail.com>
2022-10-21 01:53:17 +0100

    seo

--- app/layout/HomeLayout.js
+++ app/layout/HomeLayout.js
@@ -22,7 +22,7 @@ const HomeLayout = ({children}) => {
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
                 <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;600;700&display=swap" rel="stylesheet"></link>
             </Head>
-            <HeadSeo currentURL={router.asPath} description={'Nous sommes un mouvement citoyen international qui agit pour la promotion de l’alternance démocratique en Afrique subsaharienne.'} pageTitle={'Acceuil'} previewImage={'/groupe.jpeg'}/>
+            <HeadSeo currentURL={router.asPath} description={'Nous sommes un mouvement citoyen international qui agit pour la promotion de l’alternance démocratique en Afrique subsaharienne.'} pageTitle={'TOURNONS LA PAGE CAMEROUN | Acceuil'} previewImage={'/groupe.jpeg'}/>
             <Navbar/>
             <div className='container-layout flex flex-col justify-center mx-auto'>{children}</div>
             <Footer/>
