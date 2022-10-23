ed65c2edb6063d3c62cd50238086ef9e85146be7
Avrel OWONA <guy.owona@bhent.net>
2022-10-23 19:36:15 +0100

    footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -5,16 +5,16 @@ import { FooterItems } from '../utils/navItems';
 
 const Footer = () => {
     return (
-        <footer className='py-14 text-sm text-center text-white px-32 mt-32'>
+        <footer className='py-14 text-sm text-center text-white px-20 sm:px-32 mt-32'>
             <div className='flex flex-wrap w-full justify-center'>
                 <Link href={'/'}><img className='logo' src='/icon-navbar.png' alt='logo-footer'/></Link>
-                <div className='social-wrapper-footer justify-between flex text-black w-36 mt-12 items-center'>
+                <div className='social-wrapper-footer justify-between flex text-black w-36 mt-4 sm:mt-12 items-center'>
                         <a href=''><FaFacebook className={'hover:text-gray-600 cursor-pointer'}/></a>
-                        <a href={'#'}><FaTwitter className={'ml-5 hover:text-gray-600 cursor-pointer'}/></a>
-                        <a href={'#'}><FaYoutube className={'ml-5 hover:text-gray-600 cursor-pointer'}/></a>
+                        <a href={'#'}><FaTwitter className={'ml-3 hover:text-gray-600 cursor-pointer'}/></a>
+                        <a href={'#'}><FaYoutube className={'ml-3 hover:text-gray-600 cursor-pointer'}/></a>
                 </div>
             </div>
-            <div className='wrapper-copyright flex flex-wrap-reverse justify-center xl:justify-between py-16 font-light'>
+            <div className='wrapper-copyright flex flex-wrap-reverse justify-center lg:justify-between py-16 font-light'>
                 <span className='mt-6 lg:mt-0'>Avrel OWONA © {new Date().getFullYear()} - Tous droits réservés</span>
                 <ul className='flex'>
                         {FooterItems.map((li, index)=> {
