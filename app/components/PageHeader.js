c6d9eae26191307620e7e6d3d9332efa1b995d46
Avrel OWONA <guy.owona@bhent.net>
2022-10-28 23:23:43 +0100

    Update PageHeader.js

--- app/components/PageHeader.js
+++ app/components/PageHeader.js
@@ -4,7 +4,7 @@ const PageHeader = ({title, cover}) => {
     return (
         <div className='header-page w-full relative'>
                 <div className="h-full w-full justify-center items-center absolute top-0 flex flex-col z-40">
-                    <h3 className='text-white lg:w-2/6 font-bold text-xl lg:text-3xl text-center font-news'>{title}</h3>
+                    <h3 className='text-white w-4/6 lg:w-2/6 font-bold text-2xl lg:text-3xl text-center font-news'>{title}</h3>
                     {/*<p className='pt-10 w-10/12 lg:w-4/12 text-white text-xl font-light text-center lg:text-left'>Tournons La Page est un mouvement citoyen international qui agit pour la promotion de l’alternance démocratique en Afrique subsaharienne.</p>*/}
                 </div>
                 <div className='h-full w-full bg-black opacity-50 absolute border-red-500 top-0'></div>
