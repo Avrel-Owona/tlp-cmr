ad4bfac3509260a92d226101a2a5969152605fe9
Avrel OWONA <guy.owona@bhent.net>
2022-11-21 13:03:23 +0100

    Update HeadSeo.js

--- app/components/HeadSeo.js
+++ app/components/HeadSeo.js
@@ -1,39 +1,47 @@
 import React from "react"
 import Head from "next/head"
 
+const HeadSeo = ({ pageTitle, description, currentURL, previewImage }) => {
+	// console.log('infos', {
+	//     pageTitle : pageTitle,
+	//     description : description,
+	//     currentURL : currentURL,
+	//     previewImage : previewImage
+	// })
 
-const HeadSeo = ({pageTitle, description, currentURL, previewImage}) =>{
-    // console.log('infos', {
-    //     pageTitle : pageTitle,
-    //     description : description,
-    //     currentURL : currentURL,
-    //     previewImage : previewImage
-    // })
+	return (
+		<Head>
+			<title>Tournons La Page Cameroun | {pageTitle}</title>
+			<meta name="description" content={description} />
+			<meta property="og:title" content={pageTitle} key="ogtitle" />
+			<meta property="og:description" content={description} key="ogdesc" />
+			<meta property="og:image" content={previewImage} />
 
-    return (
-        <Head>
-                <title>Tournons La Page Cameroun | {pageTitle}</title>
-                <meta name="description" content={description} />
-                <meta property="og:title" content={pageTitle} key="ogtitle" />
-                <meta property="og:description" content={description} key="ogdesc" />
+			{/* Twitter */}
+			<meta name="twitter:site" content="tournonslapagecameroun.org" />
+			<meta name="twitter:title" content={pageTitle} />
+			<meta name="twitter:image:src" content={previewImage} />
+			<meta name="twitter:card" content="summary_large_image" key="twcard" />
+			<meta name="twitter:creator" key="twhandle" />
+			<meta name="twitter:description" content={description} />
 
-                {/* Twitter */}
-                <meta name="twitter:site" content='tournonslapagecameroun.org' />
-                <meta name="twitter:title" content={pageTitle} />
-                <meta name="twitter:image:src" content={previewImage} />
-                <meta name="twitter:card" content="summary_large_image" key="twcard" />
-                <meta name="twitter:creator" key="twhandle" />
-                <meta name="twitter:description" content={description} />
-
-                {/* Open Graph */}
-                <meta property="og:type" content='openGraphType' key="ogurl" />
-                <meta property="og:url" content={`tournonslapagecameroun.org${currentURL}`} key="ogurl" />
-                <meta property="og:image" content={previewImage} key="ogimage" />
-                <meta property="og:site_name" content='tournonslapagecameroun.org' key="ogsitename" />
-                <meta property="og:title" content={pageTitle} key="ogtitle" />
-                <meta property="og:description" content={description} key="ogdesc" />
-        </Head>
-    )
+			{/* Open Graph */}
+			<meta property="og:type" content="openGraphType" key="ogurl" />
+			<meta
+				property="og:url"
+				content={`tournonslapagecameroun.org${currentURL}`}
+				key="ogurl"
+			/>
+			<meta property="og:image" content={previewImage} key="ogimage" />
+			<meta
+				property="og:site_name"
+				content="tournonslapagecameroun.org"
+				key="ogsitename"
+			/>
+			<meta property="og:title" content={pageTitle} key="ogtitle" />
+			<meta property="og:description" content={description} key="ogdesc" />
+		</Head>
+	)
 }
 
-export default HeadSeo;
+export default HeadSeo
