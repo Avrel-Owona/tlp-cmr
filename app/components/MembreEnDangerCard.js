8e477a8ae049c4ca95c4be9b8847339f39f474b6
Avrel-Owona <guyowona01@gmail.com>
2022-12-18 12:17:30 +0100

    update of the members at risk map

--- app/components/MembreEnDangerCard.js
+++ app/components/MembreEnDangerCard.js
@@ -1,26 +1,52 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { FcDocument } from "react-icons/fc";
-import { IoMdDocument } from "react-icons/io";
-
-const HeaderAbout = () => {
-
-    return(
-        <div className="header lg:px-20 px-4 w-full relative flex justify-center">
-            <div className="flex justify-center flex-wrap-reverse sm:pt-20 lg:pb-20 w-full xl:w-11/12">
-                <div className="w-full lg:shadow-lg xl:h-full justify-center pb-10 xl:pb-0 pr-10 lg:px-10 lg:w-96 bg-white lg:relative flex flex-col lg:items-center">
-                    <span className="p-3 sm:p-4 text-center bg-orange-500 text-white font-news text-sm sm:text-xl font-normal absolute left-0 top-80 sm:left-4 lg:-left-12 sm:shadow-lg lg:-top-5">Tournons La Page Cameroun</span>
-                    <p className="flex flex-wrap items-center text-base font-light justify-start lg:justify-center xl:justify-start pt-8 xl:pt-0 lg:font-normal text-gray-900">
-                        Crée le  <span className="text-orange-500 ml-1 font-normal"> 14 Septembre 2015</span>
-                    </p>
-                    <p className="font-light text-base text-gray-700 mt-6 lg:text-center sm:w-4/6 lg:w-full">Nous sommes un mouvement citoyen international qui agit pour la promotion de l’alternance démocratique en Afrique subsaharienne.</p>
-                    <Link href="/actualites"><button className="mt-10 border-0.5 w-max border-orange-400 text-base flex items-center px-5 py-2 sm:px-12 sm:py-4 text-orange-500 font-extralight ease-in duration-100 hover:text-white hover:bg-orange-500 hover:border-orange-500">Nos ressources <IoMdDocument className="ml-2 text-sm lg:text-base"/></button></Link>
-                </div>
-                <div className="w-full lg:w-6/12 h-96 lg:shadow-lg">
-                    <img src="/header.png" className="h-full w-full object-cover" alt="cover-header"/>
-                </div>
-            </div>
-        </div>
-    )
+const MembreEnDangerCard = ({
+	date,
+	memberName,
+	link,
+	textOne,
+	textTwo,
+	country,
+	img,
+	key,
+}) => {
+	return (
+		<div
+			key={key}
+			className="lg:px-10 lg:mr-3 xl:mx-2 xl:my-20 relative lg:py-12 w-full lg:w-96 card-about-members lg:border-0.5 lg:shadow-lg my-20 lg:my-0 sm:mt-3 lg:mt-0 h-max"
+		>
+			<img src={img} alt={memberName} />
+			<h3 className="bg-orange-500 absolute top-44 right-0 h-9 text-sm flex items-center text-white justify-center font-light px-12 uppercase shadow-lg">
+				{country}
+			</h3>
+			<h1 className="text-xl mt-4 text-gray-700 lg:text-xl font-light flex items-center">
+				Arrété le <span className="text-orange-500 ml-2">{date}</span>
+			</h1>
+			<p className="list-disc text-sm lg:text-base font-extralight text-gray-600 mt-4">
+				<span className="font-medium text-gray-500">{memberName}</span>{" "}
+				{textOne}
+			</p>
+			<p className="text-sm lg:text-base font-extralight text-gray-600 mt-4">
+				{textTwo}
+			</p>
+			{link ? (
+				<>
+					<h1 className="text-orange-500 uppercase text-sm lg:text-base font-medium flex items-center mt-4">
+						Comment agir ?
+					</h1>
+					<p className="text-sm lg:text-base font-extralight ml-8 lg:ml-0 text-gray-600 mt-4">
+						Partagez sur vos réseaux le communiqué que Tournons La Page Cameroun
+						et 11 autres organisations ont signé pour exiger la libération de{" "}
+						{memberName} :{" "}
+						<a
+							className="text-orange-500"
+							href="https://bit.ly/3MLChPz"
+							target="_blank"
+						>
+							{link}
+						</a>
+					</p>
+				</>
+			) : null}
+		</div>
+	)
 }
-export default HeaderAbout;
+export default MembreEnDangerCard
--- app/components/VideoCard.js
+++ app/components/VideoCard.js
@@ -1,31 +1,52 @@
-import { useRouter } from "next/router";
-import { FaPlay } from "react-icons/fa";
-import { FiArrowRight } from "react-icons/fi";
+import { useRouter } from "next/router"
+import { FaPlay } from "react-icons/fa"
+import { FiArrowRight } from "react-icons/fi"
 
-const VideoCard = ({cover, title, link}) => {
-    const router = useRouter()
-    return (
-        <div className={`h-full w-full object-cover ${router.asPath === '/' ? 'card-video-slide' : 'card-video'} shadow-xl bg-white`}>
-                                        <div className='p-3 h-72 relative'>
-                                            <img src={cover} className='h-full w-full object-cover' alt="img-cover"/>
-                                            <a href={link} target='_blank' className="absolute h-12 shadow-lg bg-orange-500 w-12 rounded-full text-white flex items-center justify-center cursor-pointer z-30 bottom-8 left-8 hover:text-white"><FaPlay className="text-xs"/></a>
-                                        </div>
-                                        <div className='flex items-end absolute top-0 w-full h-full'>
-                                                    <div className='flex flex-col justify-between p-8 text-white w-full'>
-                                                        <h3 className='bg-orange-500 absolute bottom-60 right-0 h-9 text-xs flex items-center text-white justify-center font-light px-12 uppercase shadow-lg'>nouveau</h3> 
-                                                        <>
-                                                            <h3 className='font-semibold text-base text-gray-500'>
-                                                            {title}
-                                                            </h3>
-                                                            <div className='flex items-baseline justify-between'>
-                                                                <a href={link} target='_blank' className='flex text-sm items-center mt-3 text-gray-500 font-extralight cursor-pointer'>Voir la vidéo <FiArrowRight className='text-3xl pl-3 text-orange-500'/></a>
-                                                                {/* <span className='date-card-news hidden'>{actuality.date}</span> */}
-                                                            </div>
-                                                        </>
-                                                    </div>
-                                        </div>
-                                    </div>
-    )
+const VideoCard = ({ cover, title, link }) => {
+	const router = useRouter()
+	return (
+		<div
+			className={`h-full w-full object-cover ${
+				router.asPath === "/" ? "card-video-slide" : "card-video"
+			} shadow-xl bg-white`}
+		>
+			<div className="p-3 h-72 relative">
+				<img
+					src={cover}
+					className="h-full w-full object-cover"
+					alt="img-cover"
+				/>
+				<a
+					href={link}
+					target="_blank"
+					className="absolute h-12 shadow-lg bg-orange-500 w-12 rounded-full text-white flex items-center justify-center cursor-pointer z-30 bottom-8 left-8 hover:text-white"
+				>
+					<FaPlay className="text-xs" />
+				</a>
+			</div>
+			<div className="flex items-end absolute top-0 w-full h-full">
+				<div className="flex flex-col justify-between p-8 text-white w-full">
+					<h3 className="bg-orange-500 absolute bottom-60 right-0 h-9 text-xs flex items-center text-white justify-center font-light px-12 uppercase shadow-lg">
+						nouveau
+					</h3>
+					<>
+						<h3 className="font-semibold text-base text-gray-500">{title}</h3>
+						<div className="flex items-baseline justify-between">
+							<a
+								href={link}
+								target="_blank"
+								className="flex text-sm items-center mt-3 text-gray-500 font-extralight cursor-pointer"
+							>
+								Voir la vidéo{" "}
+								<FiArrowRight className="text-3xl pl-3 text-orange-500" />
+							</a>
+							{/* <span className='date-card-news hidden'>{actuality.date}</span> */}
+						</div>
+					</>
+				</div>
+			</div>
+		</div>
+	)
 }
 
-export default VideoCard;
+export default VideoCard
